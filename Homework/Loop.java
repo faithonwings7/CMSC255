23d7ba72e67757c3074c139ee7485f2bea2bd6ac
faithonwings7 <faithonwings7@gmail.com>
2020-11-24 13:34:33 -0500

    Add files via upload

---
+++ Homework/Loop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Loop{
+	public static void main(String [] args){
+
+		Scanner stdin = new Scanner(System.in);
+
+		n = stdin.nextInt();
+		while (n >= 1 && n <= 10){
+		    System.out.print(n);
+		}
+
+	}
+}
