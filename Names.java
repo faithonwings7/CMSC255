69d347dccb3b5a22151a6bb4df24ae964d139327
faithonwings7 <faithonwings7@gmail.com>
2020-11-24 13:52:31 -0500

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,9 @@
+public class Names 
+{
+	public static void main(String[] args)
+	{
+	System.out.println("-----------------------------------");
+	System.out.println("This program was written by Anne and Mergen");
+	System.out.println("------------------------------------");
+	}
+}
