0a6cad57379543e8b547e06273dab495a75b1f98
faithonwings7 <faithonwings7@gmail.com>
2020-11-24 13:53:55 -0500

    Add files via upload

---
+++ DaysPerMonth.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/******************************************************************************
+ *   DaysPerMonth
+ *   VCU - Computer Science Department
+ *   A program that prompts the user for a month and year (both as integers)
+ *   then displays the number of days in that month.
+ ******************************************************************************/
+class DaysPerMonth {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        /**************************************************************************
+         * Expect the user to enter a file containing numeric month numbers from the command line
+         *************************************************************************/
+        String fileName = "";
+
+        try {
+
+            fileName = args[0];
+            File inputFile = new File(fileName);
+
+            while (fileName.isEmpty()) {
+                Scanner in = new Scanner(System.in);
+                System.out.println("Enter name of file to be used: ");
+                fileName = in.nextLine();
+                inputFile = new File(fileName);
+            }
+
+            Scanner input = new Scanner(inputFile);
+
+            /***************************************************************************
+             * loop through the file taking a month and a year and determining the days for that month
+             ****************************************************************************/
+            while (input.hasNext()) {
+                int month = input.nextInt();
+                int year = input.nextInt();
+
+                /*************************************************************************
+                 * determine the number of days in the month
+                 **************************************************************************/
+                int numDays = getDays(month, year);
+
+                /*************************************************************************
+                 * display number of days in the month
+                 **************************************************************************/
+                System.out.println("There are " + numDays + " days in this month.");
+
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.print("File Not Found");
+        }
+
+    }// end main
+
+    /************************************************************
+     * method to determine the days for the given month and year
+     ************************************************************/
+    private static int getDays(int mon, int yr) {
+        int numDays = 0;
+
+        switch(mon) {
+            case 2: // February
+                numDays = 28;
+                if (yr % 4 == 0) {
+                    numDays = 29;
+                    if (yr % 100 == 0 && yr % 400 != 0)
+                        numDays = 28;
+                }
+                break;
+
+            case 4:   //April
+            case 6:   // June
+            case 9:   // September
+            case 11:  // November
+                numDays = 30;
+                break;
+
+            default: numDays = 31;  // all the rest
+        }
+        return numDays;
+    }
+
+}
