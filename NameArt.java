7f13b652870370b52b285c721f5193d1b79a1038
faithonwings7 <faithonwings7@gmail.com>
2020-11-24 12:51:47 -0500

    Add files via upload
    Project 1

---
+++ NameArt.java
@@ -0,0 +1,30 @@
+public class NameArt{
+	public static void main (String [] args){
+		System.out.println("/**************************************************************************");
+		System.out.println("*Java Program Name: NameArt");
+		System.out.println("***************************************************************************");
+		System.out.println("*Project Description: Write program to display student's name using symbols");
+		System.out.println("___________________________________________________________________________");
+		System.out.println("*Name: Anne Szarek");
+		System.out.println("*Version Date: 08/28/2019");
+		System.out.println("*Course # / Section: CMSC 255 / Section 4");
+		System.out.println("**************************************************************************/");
+		System.out.println("");
+		System.out.println("  #####################################################");
+		System.out.println("  # * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * #");
+		System.out.println("  # ~                                               ~ #");
+		System.out.println("  # *         A       N      N  N      N  EEEEEE    * #");
+		System.out.println("  # ~        A A      N N    N  N N    N  E         ~ #");
+		System.out.println("  # *       A   A     N  N   N  N  N   N  E         * #");
+		System.out.println("  # ~      AAAAAAA    N   N  N  N   N  N  EEEEE     ~ #");
+		System.out.println("  # *     A       A   N    N N  N    N N  E         * #");
+		System.out.println("  # ~    A         A  N     NN  N     NN  E         ~ #");
+		System.out.println("  # *   A           A N      N  N      N  EEEEEEE   * #");
+		System.out.println("  # ~                                               ~ #");
+		System.out.println("  # * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * #");
+		System.out.println("  #####################################################");
+
+	}
+
+}
+		
---
+++ NameArtCorrected.java
@@ -0,0 +1,35 @@
+public class NameArtCorrected{
+	public static void main (String [] args){
+		/**************************************************************************
+		*Java Program Name: NameArt
+		***************************************************************************
+		*Project Description: This project outputs my name in a unique way on the display screen
+		___________________________________________________________________________
+		*Name: Anne Szarek
+		*Version Date: 08/28/2019
+		*Course # / Section: CMSC 255 / Section 4
+		**************************************************************************/
+		System.out.println("Anne Szarek");
+		System.out.println("Project 1 - Name in Code");
+		System.out.println("This project outputs my name in a unquie way on the display screen");
+		System.out.println("08/28/2019");
+		System.out.println("CMSC 255 Section 4");
+		System.out.println("");
+		System.out.println("  #####################################################");
+		System.out.println("  # * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * #");
+		System.out.println("  # ~                                               ~ #");
+		System.out.println("  # *         A       N      N  N      N  EEEEEE    * #");
+		System.out.println("  # ~        A A      N N    N  N N    N  E         ~ #");
+		System.out.println("  # *       A   A     N  N   N  N  N   N  E         * #");
+		System.out.println("  # ~      AAAAAAA    N   N  N  N   N  N  EEEEE     ~ #");
+		System.out.println("  # *     A       A   N    N N  N    N N  E         * #");
+		System.out.println("  # ~    A         A  N     NN  N     NN  E         ~ #");
+		System.out.println("  # *   A           A N      N  N      N  EEEEEEE   * #");
+		System.out.println("  # ~                                               ~ #");
+		System.out.println("  # * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * #");
+		System.out.println("  #####################################################");
+
+	}
+
+}
+		
